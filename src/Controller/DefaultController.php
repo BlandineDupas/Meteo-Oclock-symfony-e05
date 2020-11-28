c16b9f07aa9888f1d52ab322e39f86960ed7b2fb
BlandineDupas <blandine.dupas@sfr.fr>
2020-11-28 18:29:22 +0100

    add the weather widget

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use WeatherModel;
 
@@ -12,27 +13,33 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="homepage", methods={"GET"})
      */
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
         $weatherData = WeatherModel::getWeatherData();
-        dump($weatherData);
+        $selectedCity = $session->get('selectedCity');
+        dump($selectedCity);
         return $this->render(
             'default/index.html.twig',
             [
                 'title' => 'Bienvenue sur la météo des villes !',
-                'content' => $weatherData
+                'content' => $weatherData,
+                'selectedCity' => $selectedCity
             ]
         );
     }
+
     /**
      * @Route("/montagnes", name="mountains", methods={"GET"})
      */
-    public function mountains(): Response
+    public function mountains(SessionInterface $session): Response
     {
+        $selectedCity = $session->get('selectedCity');
+
         return $this->render(
             'default/locations.html.twig',
             [
                 'title' => 'La météo des montagnes',
+                'selectedCity' => $selectedCity,
                 'content' => [
                     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eaque vitae maiores numquam suscipit nobis temporibus facilis! Facilis autem deserunt omnis enim ex voluptate facere minus tenetur. Cupiditate, illum doloremque.'
                 ]
@@ -43,17 +50,31 @@ class DefaultController extends AbstractController
     /**
      * @Route("/plages", name="beaches", methods={"GET"})
      */
-    public function beaches(): Response
+    public function beaches(SessionInterface $session): Response
     {
+        $selectedCity = $session->get('selectedCity');
+
         return $this->render(
             'default/locations.html.twig',
             [
                 'title' => 'La météo des plages',
+                'selectedCity' => $selectedCity,
                 'content' => [
                     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eaque vitae maiores numquam suscipit nobis temporibus facilis! Facilis autem deserunt omnis enim ex voluptate facere minus tenetur. Cupiditate, illum doloremque.',
                     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eaque vitae maiores numquam suscipit nobis temporibus facilis! Facilis autem deserunt omnis enim ex voluptate facere minus tenetur. Cupiditate, illum doloremque.'
                 ]
             ]
         );
+    }
+
+    /**
+     * @Route("/city/{id}", name="city_select", methods={"GET"}, requirements={ "id" = "\d+"})
+     */
+    public function selectCity(int $id, SessionInterface $session): Response
+    {
+        $cityWeather = WeatherModel::getWeatherByCityIndex($id);
+        $session->set('selectedCity', $cityWeather);
+        dump($cityWeather);
+        return $this->redirectToRoute('homepage');
     }
 }
